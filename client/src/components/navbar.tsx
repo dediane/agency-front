bdaebeffc52489aaa624a28032be0ddfb398deaa
dediane <ddecourt@student.42.fr>
2023-04-08 19:10:08 +0200

    wip

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,23 +1,28 @@
 import Link from 'next/link';
 import styles from '../styles/navbar.module.css';
 import React, {useContext, useState} from "react";
-import { Bubblegum_Sans } from 'next/font/google';
 import { Button } from './button';
+import Image from 'next/image';
 
 const Navbar = () => {
 
   return (
     <nav className={styles.navbar}>
-        <div>
-            <a href="/" className={styles.logo}>AVENTURE</a> 
+        <div className={styles.menuWrapper}>
+        <div className={styles.logo}>
+            <Image src="/logo.png" width={50} height={50} alt="logo" />
+            <a href="/">Jomnio</a> 
         </div>
-        <div>
-            <ul>
-                <li><Link className={styles.navLink} href="/">Nos Solutions</Link></li>
-                <li><Link className={styles.navLink} href="/">Nos Tarifs</Link></li>
-                <li><Link className={styles.navLink} href="/">Profitez de 1500€ remboursés</Link></li>
-                <Button text="Je profite de l'offre"/>
-            </ul>
+          <Image src="/menu-icon.svg" width={25} height={25} alt="menu hamburger" className={styles.hamburger} />
+          <div>
+              <ul>
+                  <li><Link className={styles.navLink} href="/solution">Nos Solutions</Link></li>
+                  <li><Link className={styles.navLink} href="/tarifs">Nos Tarifs</Link></li>
+                  <li><Link className={styles.navLink} href="/tarifs">Financement</Link></li>
+                  <li><Link className={styles.navLink} href="/aides">1500€ remboursés </Link></li>
+                  <Button text="Je profite de l'offre" className={styles.button}/>
+              </ul>
+          </div>
         </div>
     </nav>
   );
