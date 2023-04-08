9e45d397616ddbf453834e63c8093c474e944931
Diane <dianedecourt@gmail.com>
2023-04-09 01:43:56 +0200

    wip navbar

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,29 +1,38 @@
 import Link from 'next/link';
 import styles from '../styles/navbar.module.css';
-import React, {useContext, useState} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import { Button } from './button';
 import Image from 'next/image';
+import { useWindowSize } from "@react-hook/window-size/throttled";
 
 const Navbar = () => {
-
+    const [showNav, setShowNav] = useState(false);
+    const handleShowNav = () => {
+        setShowNav(!showNav);
+    }
+    const size = useWindowSize({ fps: 60 });
+    const isMobile = size[0] < 900;
   return (
     <nav className={styles.navbar}>
-        <div className={styles.menuWrapper}>
         <div className={styles.logo}>
             <Image src="/logo.png" width={50} height={50} alt="logo" />
             <a href="/">Jomnio</a> 
+            <Image src="/menu-icon.svg" 
+            width={25} 
+            height={25} 
+            alt="menu hamburger" 
+            className={styles.hamburger} 
+            onClick={handleShowNav}/>
         </div>
-          <Image src="/menu-icon.svg" width={25} height={25} alt="menu hamburger" className={styles.hamburger} />
-          <div>
-              <ul>
-                  <li><Link className={styles.navLink} href="/solution">Nos Solutions</Link></li>
-                  <li><Link className={styles.navLink} href="/tarifs">Nos Tarifs</Link></li>
-                  <li><Link className={styles.navLink} href="/tarifs">Financement</Link></li>
-                  <li><Link className={styles.navLink} href="/aides">1500€ remboursés </Link></li>
-                  <Button text="Je profite de l'offre" className={styles.button}/>
-              </ul>
-          </div>
-        </div>
+        {(showNav || !isMobile) ? <div className={styles.menuWrapper}>
+            <ul>
+            <li><Link className={styles.navLink} href="/solution">Nos Solutions</Link></li>
+            <li><Link className={styles.navLink} href="/tarifs">Nos Tarifs</Link></li>
+            <li><Link className={styles.navLink} href="/tarifs">Financement</Link></li>
+            <li><Link className={styles.navLink} href="/aides">1500€ remboursés </Link></li>
+            <Button text="Je profite de l'offre" className={styles.button}/>
+            </ul>
+        </div> : null}
     </nav>
   );
 };
